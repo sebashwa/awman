538deb921987a415055df85ba31cd05d26f0e8e0
sebashwa <sebashwa@mailbox.org>
2020-06-27 00:56:39 +0200

    Make txt_arg helper more clever
    It now recognizes quoted an unquoted arguments, because eat_text does
    not take care of it (which is good).

--- src/parsing/doc_structure.rs
+++ src/parsing/doc_structure.rs
@@ -5,6 +5,7 @@ use nom::{
     bytes::complete::tag,
 };
 
+use super::ManfileComponent;
 use super::helpers::{txt_arg, num_arg};
 use super::styled_text::StyledText;
 
@@ -20,13 +21,13 @@ pub struct PageTitle {
 
 type SectionHeading = StyledText;
 
-pub fn eat_page_title(line: &str) -> IResult<&str, PageTitle>  {
+pub fn eat_page_title(line: &str) -> IResult<&str, ManfileComponent>  {
     let (rest, (_, title, section, footer_middle, footer_outside, header_middle)) =
         tuple((tag(".TH"), txt_arg, opt(num_arg), opt(txt_arg), opt(txt_arg), opt(txt_arg)))(line)?;
 
     let title = PageTitle { title, section, footer_middle, footer_outside, header_middle };
 
-    Ok((rest, title))
+    Ok((rest, ManfileComponent::PageTitle(title)))
 }
 
 pub fn eat_section_heading(line: &str) -> IResult<&str, SectionHeading>  {
@@ -41,36 +42,59 @@ mod tests {
 
 
     #[test]
-    fn eat_title_returns_page_title() {
+    fn eat_page_title_returns_page_title() {
         let title_line = ".TH Title";
 
         let title = roman_txt("Title");
-        let expected = PageTitle { title, ..default_page_title() };
+        let expected = ManfileComponent::PageTitle(PageTitle {
+            title,
+            ..default_page_title()
+        });
 
         assert_eq!(eat_page_title(title_line), Ok(("", expected)));
     }
 
     #[test]
-    fn eat_title_works_with_quoted_title() {
+    fn eat_page_title_works_with_quoted_title() {
         let title_line = r#".TH "Multi Word Title""#;
 
         let title = roman_txt("Multi Word Title");
-        let expected = PageTitle { title, ..default_page_title() };
+        let expected = ManfileComponent::PageTitle(PageTitle {
+            title,
+            ..default_page_title()
+        });
 
         assert_eq!(eat_page_title(title_line), Ok(("", expected)));
     }
 
     #[test]
-    fn eat_title_works_with_all_options() {
-        let title_line = r#".TH "Multi Title" 7 "Ftr middle" "Ftr left" Header"#;
+    fn eat_page_title_works_with_some_options_quoted() {
+        let title_line = r#".TH "Multi Title" 7 FtrMiddle "Ftrleft" Header"#;
 
-        let expected = PageTitle {
+        let expected = ManfileComponent::PageTitle(PageTitle {
             title: roman_txt("Multi Title"),
             section: Some(7),
-            footer_middle: Some(roman_txt("Ftr middle")),
-            footer_outside: Some(roman_txt("Ftr left")),
+            footer_middle: Some(roman_txt("FtrMiddle")),
+            footer_outside: Some(roman_txt("Ftrleft")),
             header_middle: Some(roman_txt("Header")),
-        };
+        });
+
+
+        assert_eq!(eat_page_title(title_line), Ok(("", expected)));
+    }
+
+    #[test]
+    fn eat_page_title_works_with_all_options_unquoted() {
+        let title_line = r#".TH Title 7 FtrMiddle FtrLeft Header"#;
+
+        let expected = ManfileComponent::PageTitle(PageTitle {
+            title: roman_txt("Title"),
+            section: Some(7),
+            footer_middle: Some(roman_txt("FtrMiddle")),
+            footer_outside: Some(roman_txt("FtrLeft")),
+            header_middle: Some(roman_txt("Header")),
+        });
+
 
         assert_eq!(eat_page_title(title_line), Ok(("", expected)));
     }
--- src/parsing/helpers.rs
+++ src/parsing/helpers.rs
@@ -1,15 +1,23 @@
 use nom::{
     IResult,
     Err,
+    branch::alt,
+    bytes::complete::{tag, take_till},
     error::ErrorKind,
-    sequence::preceded,
-    character::complete::{digit1, space1},
+    sequence::{delimited, preceded},
+    character::complete::{char, digit1, space1},
 };
 
 use super::styled_text::{eat_text, StyledText};
 
 pub fn txt_arg(given: &str) -> IResult<&str, StyledText> {
-    preceded(space1, eat_text)(given)
+    let eat_quoted_arg = delimited(tag("\""), take_till(|c| c == '"'), tag("\""));
+    let eat_word = take_till(|c| c == ' ');
+
+    let (rest, arg) = preceded(space1, alt((eat_quoted_arg, eat_word)))(given)?;
+    let (_, styled_text) = eat_text(arg)?;
+
+    Ok((rest, styled_text))
 }
 
 pub fn num_arg(given: &str) -> IResult<&str, u8> {
